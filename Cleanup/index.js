2dea90d3e0fd1ce3387781fad0351f00708d7c2a
Velizar Shulev <velizar.shulev@gmail.com>
2018-04-19 20:02:41 +0300

    Add try catch

--- Cleanup/index.js
+++ Cleanup/index.js
@@ -2,24 +2,29 @@ const knex = require('knex')
 const path = require('path')
 
 module.exports = function run (context) {
-  context.log('Start cleanup')
+  try {
+    context.log('Start cleanup')
 
-  const db = require('knex')({
-    dialect: 'sqlite3',
-    connection: { filename: path.join(__dirname, '../messages.db') }
-  })
-
-  context.log('Connected to DB')
-
-  db('messages')
-    .where('created_at', '<', Date.now() / 1000 - 2 * 24 * 60 * 60)
-    .del()
-    .then(() => {
-      context.log('Cleaned up DB')
-      context.done()
+    const db = require('knex')({
+      dialect: 'sqlite3',
+      connection: { filename: path.join(__dirname, '../messages.db') }
     })
-    .catch(error => {
-      context.log('Failed to clean up DB')
-      context.done(error)
-    })
+
+    context.log('Connected to DB')
+
+    db('messages')
+      .where('created_at', '<', Date.now() / 1000 - 2 * 24 * 60 * 60)
+      .del()
+      .then(() => {
+        context.log('Cleaned up DB')
+        context.done()
+      })
+      .catch(error => {
+        context.log('Failed to clean up DB')
+        context.done(error)
+      })
+  } catch (error) {
+    context.log('An error occurred')
+    context.done(error)
+  }
 }
