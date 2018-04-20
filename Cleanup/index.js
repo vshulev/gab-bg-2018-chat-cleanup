d37951e4ac5718d18eb289efbfbb1fdab012375d
Velizar Shulev <velizar.shulev@gmail.com>
2018-04-20 13:11:20 +0300

    Remove async syntax

--- Cleanup/index.js
+++ Cleanup/index.js
@@ -1,11 +1,17 @@
 const axios = require('axios')
 
-module.exports = async function run (context) {
+module.exports = function run (context) {
   try {
     context.log('Start cleanup')
-    await axios.get(process.env.CHAT_CLEANUP_URL)
-    context.log('Finished cleanup')
-    context.done()
+    axios.get(process.env.CHAT_CLEANUP_URL)
+      .then(() => {
+        context.log('Finished cleanup')
+        context.done()
+      })
+      .catch(error => {
+        context.log('An error occurred')
+        context.done(error)
+      })
   } catch (error) {
     context.log('An error occurred')
     context.done(error)
